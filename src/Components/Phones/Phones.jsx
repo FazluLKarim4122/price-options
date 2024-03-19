cb2c3fbc40c24a773b0d1fd04dafef5cee5c9b4c
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-03-19 06:38:34 +0600

    adding a loading spinner from react awesome components

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios"
 
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Audio } from 'react-loader-spinner'
 
 
 
 const Phones = () => {
     const [phones, setPhones] = useState([])
+    const [loading, setLoading] = useState([true]) 
+    {/* by default ki hobe jokhon page ta load hobe tokhon true (active) thakbe,then data take load kore, process kore state e set kore dekhabo tokhon false(deactive) kore dibo */ }
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -25,10 +28,27 @@ const Phones = () => {
                 })
                 console.log(phonesWithFakeData)
                 setPhones(phonesWithFakeData)
+                setLoading(false)
             })
     }, [])
     return (
         <div>
+            {/*ei audio loading spinner ta dhori dekhabo jokhon data load hobe tokhon */}
+
+            {loading && 
+               <div className="flex  justify-center items-center">
+                 <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color="green"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle
+                    wrapperClass
+                />
+               </div>
+             } 
+           
             <h2 className="text-5xl">Phones:{phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
